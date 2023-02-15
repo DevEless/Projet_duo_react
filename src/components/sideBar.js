fbdee08837711c963659b78508bce0bb28504161
vic fir <vicfir@hotmail.fr>
2023-02-15 10:16:59 +0100

    structure

---
+++ src/components/sideBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const SideBar = () => {
+  return (
+    <div>
+        <ul>
+          <li>YOUR INFO</li>
+          <li>SELECT PLAN</li>
+          <li>ADD-ONS</li>
+          <li>SUMMARY</li>
+        </ul>
+    </div>
+  )
+}
