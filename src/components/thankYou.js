a98e9d8c4a8dc458b23005a8c36f580b0da6299c
vic fir <vicfir@hotmail.fr>
2023-02-16 10:16:11 +0100

    commit

--- src/components/thankYou.js
+++ src/components/thankYou.js
@@ -6,7 +6,7 @@ export const ThankYou = () => {
   return (
     <div id='thankYou' className='flex flex-col items-center h-full justify-center'>
         <img src={thankYouImg} alt="thank you" className='w-1/6 mb-8'/>
-        <h1 className='text-3xl mb-5'>Thank You!</h1>
+        <h1 className='text-3xl mb-5 font-bold'>Thank You!</h1>
         <p className='text-center'>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to  email us at support@loremgaming.com.</p>
     </div>
   )
